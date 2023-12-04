f69c4c09aa70747465cdece40e959148e56217f9
TravanR <150719363+TravanR@users.noreply.github.com>
2023-12-04 09:16:15 -0800

    Add files via upload

---
+++ States/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Called when the "Play" button is clicked
+    public void OnPlayButton ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void OnOptionsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Called when the "Quit" button clicked
+    public void OnQuitButton ()
+    {
+        Application.Quit();
+    }
+}
