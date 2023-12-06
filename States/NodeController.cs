8e742c4468431b700e614ea4fd9163191cdb4538
TravanR <150719363+TravanR@users.noreply.github.com>
2023-12-06 09:42:35 -0800

    Add files via upload

--- States/NodeController.cs
+++ States/NodeController.cs
@@ -1,10 +1,79 @@
+using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 namespace GameStates
 {
-    public class NodeController
+    public class NodeController : MonoBehaviour
     {
+        public bool canMoveLeft = false;
+        public bool canMoveRight = false;
+        public bool canMoveUp = false;
+        public bool canMoveDown = false;
+
+        public GameObject nodeLeft;
+        public GameObject nodeRight;
+        public GameObject nodeUp;
+        public GameObject nodeDown;
         
+    }
+
+    void Start()
+    {
+        RaycastHit2D[] hitsDown;
+
+        hitsDown = Physics2D.RaycastAll(Transform.position, -Vector2.up);
+
+        for (int i = 0; i < hitsDown.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsDown[i].point.y - Transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveDown = true;
+                nodeDown = hitsDown[i].collider.gameObject;
+            }
+        }
+        
+        RaycastHit2D[] hitsUp;
+
+        hitsUp = Physics2D.RaycastAll(Transform.position, -Vector2.up);
+
+        for (int i = 0; i < hitsUp.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsUp[i].point.y - Transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveUp = true;
+                nodeUp = hitsUp[i].collider.gameObject;
+            }
+        }
+        
+        RaycastHit2D[] hitsRight;
+
+        hitsRight = Physics2D.RaycastAll(Transform.position, -Vector2.right);
+
+        for (int i = 0; i < hitsRight.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsRight[i].point.y - Transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveRight = true;
+                nodeRight = hitsRight[i].collider.gameObject;
+            }
+        }
+        
+        RaycastHit2D[] hitsLeft;
+
+        hitsLeft = Physics2D.RaycastAll(Transform.position, -Vector2.up);
+
+        for (int i = 0; i < hitsLeft.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsLeft[i].point.y - Transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveLeft = true;
+                nodeLeft = hitsLeft[i].collider.gameObject;
+            }
+        }
     }
     
     public GameObject GetNodeFromDirection(string direction)
