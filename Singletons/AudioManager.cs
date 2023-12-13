3bb5f7df2009429dd2422e49c975e4a0e86a78dc
TravanR <150719363+TravanR@users.noreply.github.com>
2023-12-13 14:42:09 -0800

    Add files via upload

---
+++ Singletons/AudioManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("Audio Manager is null.");
+            }
+
+            return _instance;
+        }
+    }
+}
+
+
+
